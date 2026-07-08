5d4435c95132f7957c484b8c32483385ba63505d
sylash.k <sylash.k@vayavyalabs.com>
2026-07-08 14:03:55 +0530

    Testing for pipeline

--- .github/scripts/corellium_ci.sh
+++ .github/scripts/corellium_ci.sh
@@ -13,7 +13,7 @@ until sshpass -p "$VM_PASSWORD" ssh \
     "echo ready" >/dev/null 2>&1
 do
     echo "VM not ready yet..."
-    sleep 10
+    sleep 20
 done
 
 echo "VM is ready."
@@ -22,8 +22,36 @@ sshpass -p "$VM_PASSWORD" ssh \
     -o StrictHostKeyChecking=no \
     -o UserKnownHostsFile=/dev/null \
     -J "$SSH_PROXY" \
-    "$VM_USER@$VM_IP" <<'EOF'
-echo "Hello World!"
-hostname
-uname -a
+    "$VM_USER@$VM_IP" <<EOF
+
+export CORELLIUM_HOST="${CORELLIUM_HOST}"
+export CORELLIUM_TOKEN="${CORELLIUM_TOKEN}api"
+
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
+cat > ~/.ssh/id_ed25519 <<KEY
+$DEPLOY_KEY_GITHUB
+KEY
+
+chmod 600 ~/.ssh/id_ed25519
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+ssh -T git@github.com || true
+
+git clone git@github.com:sylavaya/ppc_test.git
+
+cd ~/ppc_test/vlabs-SiL
+
+./vlabs-SiL.sh select -n SK_S32K344
+
+./vlabs-SiL.sh test logs test_voltage_off
+
+cd ..
+
+python3 test_mail.py
+
+echo "Pipeline completed successfully."
+
 EOF
